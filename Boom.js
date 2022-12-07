fa148c35859ae53862bc34848c0d752df1767b73
RyanDoii <107153508+RyanDoii@users.noreply.github.com>
2022-12-07 15:10:44 -0500

    Add files via upload

---
+++ Boom.js
@@ -0,0 +1,12 @@
+function openYoutube(){
+    window.open("https://www.youtube.com/");
+}
+function openTwitter(){
+    window.open('https://twitter.com/doidash')
+}
+function openTwitch(){
+    window.open('https://www.twitch.tv/')
+}
+function openLinked(){
+    window.open('https://www.linkedin.com/')
+}
